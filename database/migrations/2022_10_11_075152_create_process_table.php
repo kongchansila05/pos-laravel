933655bc4d41352545e46441992594e4f059f4a2
kongchansila <chansila2222@gmail.com>
2022-11-01 14:13:24 +0700

    first commit

---
+++ database/migrations/2022_10_11_075152_create_process_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProcessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('process', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference_no');
+            $table->string('warehouse_id');
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('process');
+    }
+}
